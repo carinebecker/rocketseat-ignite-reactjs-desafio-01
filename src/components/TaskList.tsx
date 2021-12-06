6a9c6edc8dc01859b55b6c586bea5e324cf29c4f
carinebecker <caarine.m@gmail.com>
2021-12-06 10:56:03 -0300

    fix: create new task prevState

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -17,14 +17,15 @@ export function TaskList() {
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
     if (!newTaskTitle) return;
-    return setTasks([
-      ...tasks,
+    setTasks( prevState => [
+      ...prevState,
       {
         id: Math.random(),
         title: newTaskTitle,
         isComplete: false,
       },
     ]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {

4e19106989bb65d8e6fce210318dda2ec0a2f0d0
carinebecker <caarine.m@gmail.com>
2021-12-06 11:05:28 -0300

    feat: check completed task

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -17,7 +17,7 @@ export function TaskList() {
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
     if (!newTaskTitle) return;
-    setTasks( prevState => [
+    setTasks((prevState) => [
       ...prevState,
       {
         id: Math.random(),
@@ -25,11 +25,20 @@ export function TaskList() {
         isComplete: false,
       },
     ]);
-    setNewTaskTitle('');
+    setNewTaskTitle("");
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const editedTasks = tasks.map((task) =>
+      task.id == id
+        ? {
+            ...task,
+            isComplete: !task.isComplete,
+          }
+        : task
+    );
+    setTasks(editedTasks);
   }
 
   function handleRemoveTask(id: number) {
